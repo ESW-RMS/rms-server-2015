5efc8018da1519fac71864eeeb5c1b2df679b7d4
0evelyn <0evelynli@gmail.com>
2015-08-07 10:21:26 +0700

    Increased timeout to allow Gammu ample time to process texts from modem.

--- automation/rungammu.py
+++ automation/rungammu.py
@@ -2,16 +2,16 @@ import subprocess, time, signal, os
 
 print "File started"
 p = subprocess.Popen("gammurun.bat",cwd=r"C:\path\to\automationfiles",stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-time.sleep(30)
-print "30 seconds have passed."
+time.sleep(120)
+print "120 seconds have passed."
 for i in range(0,3):
     print i
     if p.terminate():
         print "Termination successful."
         break
     else:
-        print "Termination unsuccessful, retrying in 10s."
-        time.sleep(10)
+        print "Termination unsuccessful, retrying in 15s."
+        time.sleep(15)
 p.terminate()
 print "Termination unsuccessful, task will be killed."
 os.system('taskkill /fi "WindowTitle eq Gammu"')
