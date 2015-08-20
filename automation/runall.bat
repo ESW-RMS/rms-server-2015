45f496b04d365f1eead734e4ec406c56b540cb16
0evelyn <0evelyn@users.noreply.github.com>
2015-08-20 13:32:34 +0700

    Included cleanup scripts (move texts, clean CSV)

--- automation/runall.bat
+++ automation/runall.bat
@@ -2,8 +2,12 @@ echo "Run the receiver"
 cd %HOMEPATH%/path/to/automationfiles
 echo "1 - Run gammu"
 start /wait rungammu.py
+echo "1.5 - Clean inbox of old texts"
+start /wait "C:\Python27\python.exe" moveoldtexts.py
 echo "2 - Run ELK stack"
 start /wait "C:\Python27\python.exe" runel.py
+echo "2.5 - Clean up CSVs"
+start /wait "C:\Python27\python.exe" cleancsv.py
 echo "3 - Run CoreFTP upload"
 echo %time%
 call csvtosite.bat
